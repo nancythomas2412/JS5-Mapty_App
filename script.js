5b7a5be086ad757fde3d1e02f1337de8eb645998
Nancy Thomas <nancytreasathomas@gmail.com>
2024-11-29 19:38:13 +0530

    Displays Map Markers

--- script.js
+++ script.js
@@ -16,23 +16,37 @@ if (navigator.geolocation) {
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-          console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-          
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-          const coords = [latitude, longitude];
+      const coords = [latitude, longitude];
 
       //code from Leaflet site is given below
       const map = L.map('map').setView(coords, 13); //the number here is the zoom level, i think 18(maximum) and 13(minimum)
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {// it was .org before .fr/hot, this is to change the theme of map
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        // it was .org before .fr/hot, this is to change the theme of map
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      }); //on is from leaflet library, not just from js
       // Leaflet code ends here //
     },
     function () {
