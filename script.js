5de43163abb0faec7141d3dd120896f3552e97b2
Nancy Thomas <nancytreasathomas@gmail.com>
2024-12-03 18:20:10 +0530

    Renders Workout Input Form

--- script.js
+++ script.js
@@ -11,46 +11,75 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+let map, mapEvent;
 
-      const coords = [latitude, longitude];
-
-      //code from Leaflet site is given below
-      const map = L.map('map').setView(coords, 13); //the number here is the zoom level, i think 18(maximum) and 13(minimum)
-
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        // it was .org before .fr/hot, this is to change the theme of map
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Workout')
-          .openPopup();
-      }); //on is from leaflet library, not just from js
-      // Leaflet code ends here //
-    },
-    function () {
-      alert('Could not get your location!');
+class App {
+  constructor() {}
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function () {
+        alert('Could not get your location!');
+      });
     }
-  );
+  }
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+
+    //code from Leaflet site is given below
+    map = L.map('map').setView(coords, 13); //the number here is the zoom level, i think 18(maximum) and 13(minimum)
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      // it was .org before .fr/hot, this is to change the theme of map
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    //handling click on map
+    map.on('click', function (mapE) {
+      //on is from leaflet library, not just from js
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+    });
+    // Leaflet code ends here //
+  }
+  _showForm() {}
+  _toggleElevetionField() {}
+  _newWorkout() {}
 }
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  //clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+    inputCadence.value =
+      '';
+  //Display marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
